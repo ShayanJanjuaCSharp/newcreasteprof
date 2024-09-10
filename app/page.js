7790032c89577aeb7a73459f636aa428ed60ddba
Shayan I Janjua <shayanjanjua.london@gmail.com>
2024-09-10 20:39:20 +0100

    Form completed with field checking for required fields

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 export default function Home() {
-  const[prevy, setprev] =useState("");
+  //initialises the current tab counter and formvalues to bet sent to mongo
   const [currentTab, setCurrentTab] = useState(0);
   const [formValues, setFormValues] = useState({
     firstName: "",
@@ -14,6 +14,7 @@ export default function Home() {
     education:[],
 
   });
+  //initialises the education and experience objects to temporarily hold user inputted data before it is added to the formValues object
   const [experienceobj, setExp] = useState({
     role:"",
     company:"",
@@ -28,7 +29,7 @@ export default function Home() {
     end:"",
     school:""
   });
-
+//initiallises the tabs array with layout
   const tabs = [
     {
       title: "Name",
@@ -217,7 +218,7 @@ export default function Home() {
                 <div class="Company left-[408px] top-[315px] absolute text-center text-black text-2xl font-normal font-['Inter']">{exp.company}</div>
                 <div class="StartDate left-[410px] top-[352px] absolute text-center text-black text-sm font-normal font-['Inter']">{exp.start}</div>
                 <div class="EndDate left-[486px] top-[352px] absolute text-center text-black text-sm font-normal font-['Inter']">{exp.end}</div>
-                <div class="YouGainedFromIt w-[418px] h-[87px] left-[411px] top-[388px] absolute text-black text-xs font-normal font-['Inter']">This is as job description. This is where you taslk about what you did during youyr job such ats the tasks you did, the accomplishments you aschieved, and what you gained from it</div>
+                <div class="YouGainedFromIt w-[418px] h-[87px] left-[411px] top-[388px] absolute text-black text-xs font-normal font-['Inter']">{exp.jobDesc}</div>
                 <div class=" left-[477px] top-[352px] absolute text-center text-black text-sm font-normal font-['Inter']">-</div>
                 <div class="Rectangle11 w-[616px] h-1 left-[409px] top-[440px] absolute bg-[#d9d9d9]"></div>
               </div>
@@ -233,28 +234,103 @@ export default function Home() {
       title: "edu",
       content: (
         <>
-          <p><input name="educations" value={educationobj.subject} onChange={handleInputChange} placeholder="Subject..."/></p>
-          <p><input name="educationg" value={educationobj.grade} onChange={handleInputChange} placeholder="Grade/Degree..."/></p>
-          <p><input name="educationst" value={educationobj.start} onChange={handleInputChange} placeholder="Start Date..."/></p>
-          <p><input name="educatione" value={educationobj.end} onChange={handleInputChange} placeholder="End Date..."/></p>
-          <p><input name="educationsch" value={educationobj.school} onChange={handleInputChange} placeholder="School/College/University..."/></p>
+    <div class="Frame1 w-[560px] h-[214px] top-[550px] left-[390px] relative z-10">
+            <div class="WhatSYourName left-[41px] top-0 absolute text-center text-black text-[28px] font-semibold font-['Open Sans']">What education do you have?</div>
+            <div class="Firstn w-[555px] h-[70px] left-[40px] top-[56px] absolute flex flex-col">
+              <input 
+                class="shadow appearance-none border rounded w-[160px] py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="r" 
+                type="text" 
+                name="subject" 
+                placeholder="Subject" 
+                value={educationobj.subject} 
+                onChange={handleEduChange} 
+              />
+              <input 
+                class="shadow appearance-none border rounded w-[170px] py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="c" 
+                type="text" 
+                name="grade" 
+                placeholder="Grade" 
+                value={educationobj.grade} 
+                onChange={handleEduChange} 
+              />
+              <div class='flex-wrap flex'>
+              <input 
+                class="shadow appearance-none border rounded w-[100px] py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="s" 
+                type="text" 
+                name="start" 
+                placeholder="Start Date" 
+                value={educationobj.start} 
+                onChange={handleEduChange} 
+              />
+              <input 
+                class="shadow appearance-none border rounded w-[100px] py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="e" 
+                type="text" 
+                name="end" 
+                placeholder="End Date" 
+                value={educationobj.end} 
+                onChange={handleEduChange} 
+              />
+              </div>
+              <input 
+                class="shadow appearance-none border rounded w-[300px] py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="j" 
+                type="text" 
+                name="school" 
+                placeholder="School/College/University" 
+                value={educationobj.school} 
+                onChange={handleEduChange} 
+              />
+              <button 
+                type="button"
+                id="subedu"
+                name="subedu"
+                onClick={handleEduSubmit} class="Rectangle7 w-[84px] h-[25px] bg-[#0057bb] rounded-[5px]">Submit</button>
+            </div>
+          </div>
+          <div class="absolute  overflow-auto w-[650px] h-[800px] flex flex-col z-100">
+          {formValues.education.map((edu, index) => {
+            return(
+              <div key='index ' class='w-[600px] h-[500px]' >
+                <div class="Role left-[410px] top-[276px] absolute text-center text-black text-[32px] font-normal font-['Inter']">{edu.subject}</div>
+                <div class="Company left-[408px] top-[315px] absolute text-center text-black text-2xl font-normal font-['Inter']">{edu.grade}</div>
+                <div class="StartDate left-[410px] top-[352px] absolute text-center text-black text-sm font-normal font-['Inter']">{edu.start}</div>
+                <div class="EndDate left-[486px] top-[352px] absolute text-center text-black text-sm font-normal font-['Inter']">{edu.end}</div>
+                <div class="YouGainedFromIt w-[418px] h-[87px] left-[411px] top-[388px] absolute text-black text-xs font-normal font-['Inter']">{edu.school}</div>
+                <div class=" left-[477px] top-[352px] absolute text-center text-black text-sm font-normal font-['Inter']">-</div>
+                <div class="Rectangle11 w-[616px] h-1 left-[409px] top-[440px] absolute bg-[#d9d9d9]"></div>
+              </div>
+              
+          );
+          })}
+          </div>
+          
         </>
       ),
     },
   ];
 
-  // Handle input change
+  // Handles input change for first 3 tabs
   function handleInputChange(e) {
     const { name, value } = e.target;
     setFormValues((prev) => ({ ...prev, [name]: value }));
   }
-
+//handles input change for tab 4
   function handleExpChange(e) {
     const { name, value } = e.target;
     setExp((prev) => ({ ...prev, [name]: value }));
     
   }
-
+//handles input change for tab 5
+  function handleEduChange(e) {
+    const { name, value } = e.target;
+    setEdu((prev) => ({ ...prev, [name]: value }));
+    
+  }
+//handles the addition of the form values on the experience tab to the experience array
   function handleExpSubmit(){
     let arr = formValues.experience
     arr.push(experienceobj)
@@ -263,7 +339,16 @@ export default function Home() {
       experience:arr
     }))
   }
-
+//the same as the previous but for the education array and tab
+  function handleEduSubmit(){
+    let arr = formValues.education
+    arr.push(educationobj)
+    setFormValues(prev =>({
+      ...prev,
+      education:arr
+    }))
+  }
+//handles the addition of a skill to the skills array in form values
   function handleSkillChange(e){
     let arr = formValues.skills
     arr.push(formValues.skill)
@@ -272,7 +357,7 @@ export default function Home() {
       skills:arr
     }))
   }
-
+//handles the deletion of a skill within the skill array
   function handleSkillDelete(i){
     let arr = formValues.skills
     delete arr[i]
@@ -281,34 +366,58 @@ export default function Home() {
       skills:arr
     }))
   }
-
-  // Show the tab content
+//shows the content of the current tab
   function showTab(n) {
     return tabs[n].content;
   }
-
-  // Handle next/previous functionality
+//calls validate fields and then checks if the form is on the last tab before submitting, or changes the tab to the next tab
   function nextPrev(n) {
-    /*if (n === 1 && !validateForm()) {
-      return;}*/
-    setCurrentTab((prev) => prev + n);
-  }
-
-  // Validate form fields
-  function validateForm() {
-  }
-
-  /*function prevNext(n){
-    
-    nextPrev(n)
-    if(currentTab===0){
-      setprev("Rectangle5 w-[163px] h-[66px] left-[454px] top-[761px] absolute bg-white rounded-[117px]")
+    if(validateFields(currentTab)){
+      if (n === 4) {
+        submitToMongo()
+        return;
+      }
+      else{
+        setCurrentTab((prev) => prev + n);
+      }
+      
     }
     else{
-      setprev("Rectangle5 w-[163px] h-[66px] left-[454px] top-[761px] absolute bg-[#b6b6b6] rounded-[117px]")
+      //send error to user saying to complete all fields
     }
-  }*/
-  // Update button label
+  }
+//checks the current tab and checks if all fields within that tab have been filled, only checks the required ones
+  function validateFields(n) {
+    switch(n){
+      case(0):
+        if(formValues.firstName === "" || formValues.lastName===""){
+          return false;
+        }
+        return true;
+        break;
+
+      case(1):
+        if(formValues.postcode === "" ){
+          return false;
+        }
+        break;
+
+      case(2):
+        if(formValues.skills.length === 0){
+          return false;
+        }
+        break;
+
+      default:
+        return true;
+    }
+    return true;
+  }
+  //function to add the formvalues as a document to the Mongo Database
+  async function submitToMongo(){
+    //Area to submit data from FormValues to Mongo document
+  }
+//changes the text of the next button to Submit on the final tab
   const nextButtonLabel = currentTab === tabs.length - 1 ? "Submit" : "Next";
 
   return (
@@ -322,7 +431,7 @@ export default function Home() {
   <div class="CreateYourProfile w-[1440px] left-0 top-[189px] absolute text-center text-[#0378ff] text-5xl font-bold font-['Open Sans']">Create your profile</div>
   <div class="Rectangle5 w-[163px] h-[66px] left-[454px] top-[761px] absolute bg-[#b6b6b6] rounded-[117px]"></div>
   <div class="Rectangle6 w-[163px] h-[66px] left-[817px] top-[758px] absolute bg-[#3b96ff] rounded-[117px]"></div>
-  <button class="Next w-[74px] h-[38px] left-[862px] top-[772px] absolute text-center text-white text-[28px] font-regular font-['Open Sans']" type="button" id="nextBtn" onClick={() => nextPrev(1)}>Next</button>
+  <button class="Next w-[74px] h-[38px] left-[862px] top-[772px] absolute text-center text-white text-[28px] font-regular font-['Open Sans']" type="button" id="nextBtn" onClick={() => nextPrev(1)}>{nextButtonLabel}</button>
   <button class="Previous w-[156px] h-[38px] left-[457px] top-[772px] absolute text-center text-white text-[28px] font-regular  font-['Open Sans']" type="button" id="prevBtn" onClick={() => nextPrev(-1)} disabled={currentTab === 0}>{currentTab===0? '' : 'Previous'}</button>
   </div>  
     </form>
